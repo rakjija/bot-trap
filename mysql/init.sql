5ff37d8ffabd7de00aafcc27406017b4c6e31cb8
rakjija <minjai100111@gmail.com>
2025-04-28 07:29:31 +0900

    chore: init.sql 수정 (초기 사용자 권한 및 DB 세팅 개선)

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,1 +1,7 @@
-USE goboard_board;
+USE goboard;
+
+CREATE USER IF NOT EXISTS 'goboard-user'@'%' IDENTIFIED BY 'goboard-pass';
+
+GRANT ALL PRIVILEGES ON `goboard`.* TO 'goboard-user'@'%';
+
+FLUSH PRIVILEGES;
