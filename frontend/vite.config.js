42ed0d29ba7357a22e8a37b89c3eece56779d042
rakjija <minjai100111@gmail.com>
2025-04-28 07:28:33 +0900

    feat: 프론트엔드 요청 수정 및 nginx 디렉터리 분리

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	base: "./",
 	plugins: [react()],
-	server: {
-		proxy: {
-			"/api/v1": {
-				target: "http://localhost:8080",
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api\/v1/, ""),
-			},
-		},
+	build: {
+		outDir: "dist",
 	},
 });
